6e7b3616f8873d732b83fcdfdfe1b033ca9f5eb2
MathiasDanzeisen <matze@specialo.de>
2021-11-19 20:56:09 +0100

    Warm up task remove segmem fault

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    //delete _chatBot;  //Warmup Task, chatbot is created & deleted in chatbot logic 
 
     ////
     //// EOF STUDENT CODE
